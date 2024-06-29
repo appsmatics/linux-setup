e6a08c8601897eb2812ac5adc03d21c6776d6afc
Mani Harihara <mani@appsmatics.com>
2024-06-29 08:26:33 +0530

    define a folder variable as it is used in multiple places

--- installs/lazygit.sh
+++ installs/lazygit.sh
@@ -1,18 +1,25 @@
 ### lazygit setup:
 ##### Download the required version from https://github.com/jesseduffield/lazygit/releases
 
+OPT_DIR="$HOME/opt"
+
 ##### installing lazygit
 LAXYGIT_VERSION="0.42.0"
 LAZYGIT_X86="lazygit_$(LAZYGIT_VERSION)_Linux_x86_64"
 LAZYGIT_TAR_DOWNLOAD_LINK="https://github.com/jesseduffield/lazygit/releases/download/v$LAZYGIT_VERSION/$LAZYGIT_X86.tar.gz"
-if [[ -d "$HOME/opt/lazygit/$LAZYGIT_X86" ]]
+LAZYGIT_X86_FOLDER="$OPT_DIR/lazygit/$LAZYGIT_X86"
+
+if [[ -d "$LAZYGIT_X86_FOLDER" ]]
 then
   echo "lazygit v$LAZYGIT_VERSION already available .. skipping"
 else
   echo "installing lazygit v$LAZYGIT_VERSION"
-  mkdir -p "$HOME/opt/lazygit/$LAZYGIT_X86"
-  #mkdir -p $HOME/opt/lazygit/lazygit_0.42.0_Linux_x86_64
-  mv "$HOME/Downloads/$LAZYGIT_X86.tar.gz" "$HOME/opt/lazygit/$LAZYGIT_X86"
-  cd "$HOME/opt/lazygit/$LAZYGIT_X86"
+  mkdir -p "$LAZYGIT_X86_FOLDER"
+  #mkdir -p $OPT_DIR/lazygit/lazygit_0.42.0_Linux_x86_64
+  mv "$HOME/Downloads/$LAZYGIT_X86.tar.gz" "$LAZYGIT_X86_FOLDER"
+  cd "$LAZYGIT_X86_FOLDER"
   tar -xf "$LAZYGIT_X86.tar.gz"
+  cd "$OPT_DIR/lazygit"
+  rm current
+  ln -s "$LAZYGIT_X86_FOLDER" current
 fi
