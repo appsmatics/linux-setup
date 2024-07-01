e27ae4f5b378e8963c1271566a9b80daedaec5b5
Mani Harihara <mani@appsmatics.com>
2024-07-01 09:54:33 +0530

    commiting the node version being used atm

--- installs/nodejs.sh
+++ installs/nodejs.sh
@@ -1,7 +1,11 @@
 ### nodejs setup:
 ##### download nodejs tar file(https://nodejs.org/dist/v20.15.0/node-v20.15.0-linux-x64.tar.xz)
 
+# TODO; use this version
+NODEJS_VERSION="18.17.1"
+
 NODEJS_DIR="$OPT_DIR/nodejs"
+
 mkdir -p $NODEJS_DIR
 mv $HOME/Downloads/node-v20.15.0-linux-x64.tar.xz $NODEJS_DIR
 cd $NODEJS_DIR
