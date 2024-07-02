f7c34e5e9f40a8349fa1ea02cd86c89ebbbc4a10
Ajith K <ajithkaruppiah23@gmail.com>
2024-07-02 18:28:23 +0530

    nodejs setup script
    Complete the nodejs installation using sh file

--- installs/nodejs.sh
+++ installs/nodejs.sh
@@ -1,13 +1,31 @@
 ### nodejs setup:
-##### download nodejs tar file(https://nodejs.org/dist/v20.15.0/node-v20.15.0-linux-x64.tar.xz)
-
-# TODO; use this version
+##### download nodejs tar file(https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz)
+OPT_DIR="$HOME/opt"
 NODEJS_VERSION="18.17.1"
-
+#cur_ver:"18.17.1"
 NODEJS_DIR="$OPT_DIR/nodejs"
+NODEJS_X64="node-v${NODEJS_VERSION}-linux-x64"
+NODEJS_FOLDER="$NODEJS_DIR/$NODEJS_X64"
+NODEJS_TAR_DOWNLOAD_LINK=https://nodejs.org/dist/v${NODEJS_VERSION}/node-v${NODEJS_VERSION}-linux-x64.tar.xz
 
-mkdir -p $NODEJS_DIR
-mv $HOME/Downloads/node-v20.15.0-linux-x64.tar.xz $NODEJS_DIR
+if [[ -d "$NODEJS_FOLDER" ]]
+then
+  echo ".. lazygit version $NODEJS_VERSION already exists"
+else
+ mkdir -p $NODEJS_DIR
+ cd $NODEJS_DIR
+ echo "...downloading nodejs $NODEJS_VERSION"
+ curl -LO  $NODEJS_TAR_DOWNLOAD_LINK
+ tar -xf node-v${NODEJS_VERSION}-linux-x64.tar.xz
+fi
+
+# Always update the current symlink
+echo ".. resetting current symlink"
 cd $NODEJS_DIR
-tar -xf node-v20.15.0-linux-x64.tar.xz
-ln -s node-v20.15.0-linux-x64 current
+if [[ -d "current" ]]
+then
+  rm current
+fi
+ln -s node-v${NODEJS_VERSION}-linux-x64 current
+echo ".. lazygit $NODEJS_VERSION  .. current symlink updated"
+
