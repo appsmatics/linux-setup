46aedf15ef1395e55e09b6fe8135c4b022c56add
Ajith K <ajithkaruppiah23@gmail.com>
2024-07-02 23:44:14 +0530

    zoom and chrome script installation
    Add scripts to install zoom and chrom

--- installs/zoom_chrome.sh
+++ installs/zoom_chrome.sh
@@ -1,5 +1,16 @@
-### installing zoom:
-sudo dpkg -i zoom_amd64.deb
+### installing zoom
 apt --fix-broken install
 sudo apt --fix-broken install
-sudo dpkg -i zoom_amd64.deb
+#sudo dpkg -i zoom_amd64.deb
+echo "zoom install started"
+wget https://zoom.us/client/latest/zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+echo "zoom installed successfully"
+
+### installing chrome
+echo "chrome install started"
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+echo "chrome installed successfully"
