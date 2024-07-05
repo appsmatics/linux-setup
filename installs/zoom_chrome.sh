8690a7e4a4a91ab3d2a1bbd1cacce15de58d5741
Ajith K <ajithkaruppiah23@gmail.com>
2024-07-05 16:24:48 +0530

    separate folder for zoom and chrome
    Add separate folders to download and install deb files

--- installs/zoom_chrome.sh
+++ installs/zoom_chrome.sh
@@ -1,14 +1,25 @@
 ### installing zoom
+DEST_DIR="$HOME/opt"
+
+ZOOM_FOLDER="$DEST_DIR/zoom"
 apt --fix-broken install
 sudo apt --fix-broken install
 #sudo dpkg -i zoom_amd64.deb
 echo "zoom install started"
+mkdir -p "$ZOOM_FOLDER"
+cd $ZOOM_FOLDER
+echo ".. downloading zoom deb"
 curl https://zoom.us/client/latest/zoom_amd64.deb
 sudo apt install ./zoom_amd64.deb
 echo "zoom installed successfully"
 
 ### installing chrome
+CHROME_FOLDER="$DEST_DIR/chrome"
+
 echo "chrome install started"
+mkdir -p "$CHROME_FOLDER"
+cd $CHROME_FOLDER
+echo ".. downloading chrome deb"
 curl -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
