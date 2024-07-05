5dd4b0edb8922131cc7d118dedf5b4863a3d95eb
Ajith K <ajithkaruppiah23@gmail.com>
2024-07-05 08:59:25 +0530

    Android sdk full setup
    Add script to install allrequired android sdk tools for flutter development.
    pending:android license status unknown

--- installs/flutter.sh
+++ installs/flutter.sh
@@ -5,103 +5,129 @@
 OPT_DIR="$HOME/opt"
 FLUTTER_VERSION="3.22.2"
 FLUTTER_FOLDER="$OPT_DIR/flutter"
+FLUTTER_VERSION_FOLDER="$FLUTTER_FOLDER/flutter-$FLUTTER_VERSION"
 FLUTTER_TAR_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 
 
-if [[ -d "$FLUTTER_FOLDER" ]]
+
+if [[ -d "$FLUTTER_VERSION_FOLDER" ]]
 then
   echo "flutter $FLUTTER_VERSION already available .. skipping"
 else
-  curl -LO  $FLUTTER_TAR_DOWNLOAD_LINK
-  echo "installing flutter $FLUTTER_VERSION"
-  mkdir  -p "$FLUTTER_FOLDER"
-  mv $HOME/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz $FLUTTER_FOLDER
+  mkdir  -p "$FLUTTER_FOLDER" 
   cd $FLUTTER_FOLDER
+  echo "downloading flutter $FLUTTER_VERSION"
+  #curl -LO  $FLUTTER_TAR_DOWNLOAD_LINK
+  echo "installing flutter $FLUTTER_VERSION"
+  echo "untaring the file started..."
   tar -xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
-  if [[ -d "current" ]]
-  then
-  rm current
-  fi
-  ln -s flutter current
+  mv flutter $FLUTTER_VERSION_FOLDER
 fi
 
+# Always update the current symlink
+echo ".. resettting current symlink"
+cd "$FLUTTER_FOLDER"
+if [[ -d "current" ]]
+then
+  rm current
+fi
+ln -s $FLUTTER_VERSION_FOLDER current
+echo ".. flutter $FLUTTER_VERSION  .. current symlink updated"
+cd
 
 
   
 ### android sdk setup: 
-mkdir -p $OPT_DIR/android/sdk/platform-tools
-cd $OPT_DIR/android/sdk/platform-tools
+echo "starting android sdk setup.."
+mkdir -p $OPT_DIR/android/sdk
+cd $OPT_DIR/android/sdk
 
 PLATFORM_TOOLS_VERSION="34.0.1"
 
-#LAZYGIT_X86="lazygit_${LAZYGIT_VERSION}_Linux_x86_64"
 PLATFORM_TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip"
 
 #### download the platfom tools from 
+if [[ -a "platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip" ]]
+then
+  echo "platrform-tools version $PLATFORM_TOOLS_VERSION already exists..skipping"
+else
 curl -LO $PLATFORM_TOOLS_DOWNLOAD_LINK
-#mv $HOME/Downloads/platform-tools-latest-linux.zip $OPT_DIR/android/sdk
-unzip platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip && mv platform-tools platform-tools-${PLATFORM-TOOLS-VERSION}
-
+fi
+unzip  platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip
+#mv platform-tools platform-tools-${PLATFORM_TOOLS_VERSION}
 #### use appropriate version
 #mv platform-tools platform_tools_x.y.z
-ln -s platform-tools_${PLATFORM_TOOLS_VERSION} current
+#ln -s platform-tools-${PLATFORM_TOOLS_VERSION} current
+flutter config --android-sdk="$HOME/opt/android/sdk"
+flutter doctor --android-licenses
+flutter doctor -v
 
-flutter doctor
-  
- << com 
-### android sdk not located error started:
-  
-flutter config --android-sdk /home/ajith/opt/android/sdk/current
-flutter doctor
-vim .bashrc
-flutter doctor
-vim .bashrc
-flutter doctor
-cd opt/android/sdk
-ll
-rm current
-ln -s platform_tools_35.0.1
-ln -s platform_tools_35.0.1 current
+TOOLS_VERSION="25.2.3"
+
+#LAZYGIT_X86="lazygit_${LAZYGIT_VERSION}_Linux_x86_64"
+TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/tools_r${TOOLS_VERSION}-linux.zip"
   
 ### android tools download:
-curl https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
-ls
-unzip tools_r25.2.3-linux.zip
-cd
-vim .bashrc
-  
+if [[ -a "tools_r${TOOLS_VERSION}-linux.zip" ]]
+then
+  echo "tools version $TOOLS_VERSION already exists..skipping"
+else
+#curl https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
+curl -LO $TOOLS_DOWNLOAD_LINK
+fi
+unzip  tools_r${TOOLS_VERSION}-linux.zip
+ln -s tools current
+
+
 ### jdk not found error fix:
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 
+
+
 ### continuing android sdk location error fix,cmdline-tools missing:  
-cd opt/android/sdk/tools
-flutter doctor
-path/to/sdkmanager --install "cmdline-tools;latest
-~/opt/android/sdk --install "cmdline-tools;latest
+cd opt/android/sdk/tools/bin
+sdkmanager --install "cmdline-tools;latest"
 flutter doctor
   
 
 
 ### commandlinetools download and installation:(cmdline-tools component is missing fix)
-ref link:https://linuxopsys.com/install-android-sdk-without-android-studio-on-ubuntu
-commandlinetools download link:https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+#ref link:https://linuxopsys.com/install-android-sdk-without-android-studio-on-ubuntu
+#commandlinetools download link:https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
 cd opt/android/sdk
-unzip commandlinetools-linux-11076708_latest.zip 
+echo "latest command line tools are downloading..."
+if [[ -a "commandlinetools-linux-11076708_latest.zip" ]]
+then
+  echo "commandlinetools version latest version already exists..skipping"
+else
+curl -LO "https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip"
+fi
+unzip  commandlinetools-linux-11076708_latest.zip 
 cd cmdline-tools/
 mkdir tools 
 mv -i * tools
+cho "command line tools are ready..."
+
 
 ### required sdk tools installation command:
+echo "installing required sdk tools.."
 flutter config --android-sdk="$OPT_DIR/android/sdk"
-~/opt/android/sdk/tools/bin/sdkmanager "platform-tools" "platforms;android-30" "build-tools; 30.0.3"
-  
+cd opt/android/sdk/cmdline-tools/tools/bin
+sdkmanager "platforms;android-30" "build-tools; 29.0.3"
+sdkmanager --licenses -y
+echo "sdk tools(platforms;android-30" "build-tools; 29.0.3) installed.."
+
+
+
 ### java version missing error:
 apt install openjdk-17-jdk openjdk-17-jre
 sudo apt install openjdk-17-jdk openjdk-17-jre
+
+flutter doctor -v
   
-  
+ << com   
 ### Connected device (the doctorcheck is crashed):
 Downgraded the platform-tools version from 35.0.1 to 34.0.0
 Unable to run "adb", check your Android SDK installation and ANDROID_HOME environment variable: /home/ajith/opt/android/sdk/platform-tools/adb
