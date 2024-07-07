25891bc0e1e740b729d211ae7096fd2a4bdb79cb
Mani Harihara <mani@appsmatics.com>
2024-07-07 08:20:20 +0530

    hoist version vars to top and standardise echo

--- installs/flutter.sh
+++ installs/flutter.sh
@@ -1,12 +1,22 @@
-### flutter setup:
-##### download flutter latest version tar file from flutter page (https://storage.googleapis.com/   flutter_infra_release/releases/stable/linux/flutter_linux_3.22.2-stable.tar.xz)
-
-##### installing flutter
-FLUTTER_VERSION="3.22.2"
+# setup vars needed:
+FLUTTER_VERSION="${FLUTTER_VERSION:-"3.22.2"}"
 DEST_DIR="${DEST_DIR:-"$HOME/opt"}"
+
+PLATFORM_TOOLS_VERSION="34.0.1"
+TOOLS_VERSION="25.2.3"
+
+# installing flutter
+echo "FLUTTER_VERSION=$FLUTTER_VERSION"
+echo "DEST_DIR=$DEST_DIR"
+echo "PLATFORM_TOOLS_VERSION=$PLATFORM_TOOLS_VERSION"
+echo "TOOLS_VERSION=$TOOLS_VERSION"
+
 FLUTTER_FOLDER="$DEST_DIR/flutter"
 FLUTTER_VERSION_FOLDER="$FLUTTER_FOLDER/flutter-$FLUTTER_VERSION"
 FLUTTER_TAR_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
+
+PLATFORM_TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip"
+TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/tools_r${TOOLS_VERSION}-linux.zip"
 
 
 
@@ -22,32 +32,30 @@ else
   echo "untaring the file started..."
   echo "extracting flutter-${FLUTTER_VERSION} tar file..."
   tar -xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+  # TODO: is this correct?
   mv flutter $FLUTTER_VERSION_FOLDER
 fi
 
 # Always update the current symlink
-echo ".. resetting current symlink"
+echo "resetting current symlink"
 cd "$FLUTTER_FOLDER"
 if [[ -d "current" ]]
 then
   rm current
 fi
 ln -s $FLUTTER_VERSION_FOLDER current
-echo ".. flutter $FLUTTER_VERSION  .. current symlink updated"
+echo "flutter $FLUTTER_VERSION  .. current symlink updated"
 cd
 
 
   
-### android sdk setup: 
+# android sdk setup: 
 echo "starting android sdk setup.."
 mkdir -p $DEST_DIR/android/sdk
 cd $DEST_DIR/android/sdk
 
-PLATFORM_TOOLS_VERSION="34.0.1"
 
-PLATFORM_TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip"
-
-#### download the platfom tools from 
+# download the platfom tools from 
 if [[ -a "platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip" ]]
 then
   echo "platrform-tools version $PLATFORM_TOOLS_VERSION already exists..skipping"
@@ -57,20 +65,19 @@ curl -LO $PLATFORM_TOOLS_DOWNLOAD_LINK
 fi
 echo "unzipping platform-tools_r${PLATFORM_TOOLS_VERSION} tar file..."
 unzip  platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip
+
 #mv platform-tools platform-tools-${PLATFORM_TOOLS_VERSION}
-#### use appropriate version
+
+# use appropriate version
 #mv platform-tools platform_tools_x.y.z
 #ln -s platform-tools-${PLATFORM_TOOLS_VERSION} current
 flutter config --android-sdk="$HOME/opt/android/sdk"
 flutter doctor --android-licenses
 flutter doctor -v
 
-TOOLS_VERSION="25.2.3"
 
-#LAZYGIT_X86="lazygit_${LAZYGIT_VERSION}_Linux_x86_64"
-TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/tools_r${TOOLS_VERSION}-linux.zip"
   
-### android tools download:
+## android tools download:
 if [[ -a "tools_r${TOOLS_VERSION}-linux.zip" ]]
 then
   echo "tools version $TOOLS_VERSION already exists..skipping"
@@ -112,9 +119,8 @@ flutter config --android-sdk="$DEST_DIR/android/sdk"
 flutter doctor -v
 
 
-#after we need to do these step manually
 ### required sdk tools installation command:
-echo "after we need to do these step manually to install required sdk tools"
+echo "do these step manually to install required sdk tools"
 echo "cd opt/android/sdk/cmdline-tools/latest/bin"
 echo "sdkmanager --list"
 echo "find the available platforms and build-tools version above"
@@ -124,8 +130,6 @@ echo "sdkmanager --licenses"
 
 
   
- 
-
 ### important: setting env path for android setup
 # android sdk version path
 #export PATH="$DEST_DIR/android/sdk/current/bin:$PATH"
