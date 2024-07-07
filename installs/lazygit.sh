ad147b47398c488fbcc5d60031f0e7ef42206630
Mani Harihara <mani@appsmatics.com>
2024-07-07 08:23:01 +0530

    hoist need vars to top

--- installs/lazygit.sh
+++ installs/lazygit.sh
@@ -1,10 +1,14 @@
-### lazygit setup:
-##### Download the required version from https://github.com/jesseduffield/lazygit/releases
-##### installing lazygit
-LAZYGIT_VERSION="0.42.0"
+# setup vars needed:
+LAZYGIT_VERSION="${LAZYGIT_VERSION:="0.42.0"}"
 DEST_DIR="${DEST_DIR:-"$HOME/opt"}"
 
-LAZYGIT_X86="lazygit_${LAZYGIT_VERSION}_Linux_x86_64"
+
+
+# installing lazygit
+echo "LAZYGIT_VERSION=$LAZYGIT_VERSION"
+echo "DEST_DIR=$DEST_DIR"
+
+LAZYGIT_X86="lazygit_${LAZYGIT_VERSION}Linux_x86_64"
 LAZYGIT_TAR_DOWNLOAD_LINK="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 
 #"https://github.com/jesseduffield/lazygit/releases/download/v$LAZYGIT_VERSION/lazygit_$(LAZYGIT_VERSION)_Linux_arm64.tar.gz"
@@ -14,24 +18,24 @@ LAZYGIT_X86_FOLDER="$LAZYGIT_FOLDER/$LAZYGIT_X86"
 
 if [[ -d "$LAZYGIT_X86_FOLDER" ]]
 then
-  echo ".. lazygit version $LAZYGIT_VERSION already exists"
+  echo "lazygit version $LAZYGIT_VERSION already exists"
 else
-  echo ".. installing lazygit $LAZYGIT_VERSION"
+  echo "installing lazygit $LAZYGIT_VERSION"
   mkdir -p "$LAZYGIT_X86_FOLDER"
   cd $LAZYGIT_FOLDER
-  echo ".. downloading lazygit $LAZYGIT_VERSION"
+  echo "downloading lazygit $LAZYGIT_VERSION"
   curl -LO  $LAZYGIT_TAR_DOWNLOAD_LINK
-  echo "..extracting lazygit tar file..."
+  echo "extracting lazygit tar file..."
   tar -xf lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz -C $LAZYGIT_X86_FOLDER
-  echo ".. lazygit version $LAZYGIT_VERSION installed"
+  echo "lazygit version $LAZYGIT_VERSION installed"
 fi
 
 # Always update the current symlink
-echo ".. resetting current symlink"
+echo "resetting current symlink"
 cd "$DEST_DIR/lazygit"
 if [[ -d "current" ]]
 then
   rm current
 fi
 ln -s $LAZYGIT_X86 current
-echo ".. lazygit $LAZYGIT_VERSION  .. current symlink updated"
+echo "lazygit $LAZYGIT_VERSION  current symlink updated"
