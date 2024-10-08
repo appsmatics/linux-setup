3ccec1de0a249e316015d32ecafb3cd78de87f5c
Mani Harihara <mani@appsmatics.com>
2024-10-08 11:23:15 -0700

    fix some bugs in flutter2.sh

--- installs/flutter2.sh
+++ installs/flutter2.sh
@@ -1,21 +1,23 @@
 # setup vars needed:
-FLUTTER_VERSION="${FLUTTER_VERSION:-"3.24.3"}"
-DEST_DIR="${DEST_DIR:-"$HOME/opt"}"
+# FLUTTER_VERSION="${FLUTTER_VERSION:-"3.24.3"}"
+# DEST_DIR="${DEST_DIR:-"$HOME/opt"}"
 
-echo "Flutter version = $FLUTTER_VERSION"
-echo "Android version = $ANDROID_VERSION"
+# echo "Flutter version = $FLUTTER_VERSION"
+# echo "Android version = $ANDROID_VERSION"
 
 
 
 
 #PLATFORM_TOOLS_VERSION="34.0.1"
-ANDROID_VERSION="34"
-BUILD_TOOLS_VERSION="34.0.0"
+# ANDROID_VERSION="34"
+# BUILD_TOOLS_VERSION="34.0.0"
 #TOOLS_VERSION="25.2.3"
 
 # installing flutter
 echo "FLUTTER_VERSION=$FLUTTER_VERSION"
 echo "DEST_DIR=$DEST_DIR"
+echo "OS=$OS, REQUIRED_OS=$REQUIRED_OS"
+
 #echo "PLATFORM_TOOLS_VERSION=$PLATFORM_TOOLS_VERSION"
 #echo "TOOLS_VERSION=$TOOLS_VERSION"
 
@@ -23,22 +25,19 @@ echo "DEST_DIR=$DEST_DIR"
 FLUTTER_FOLDER="$DEST_DIR/flutter"
 FLUTTER_VERSION_FOLDER="$FLUTTER_FOLDER/flutter-$FLUTTER_VERSION"
 FLUTTER_DOWNLOAD_LINK=""
-OS=$(uname -s)
-if [ $OS = "Darwin" ]
-then
-echo "macos $OS"
-FLUTTER_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_${FLUTTER_VERSION}-stable.zip"
+
+
+if [ $REQUIRED_OS = "mac" ]; then
+  FLUTTER_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_${FLUTTER_VERSION}-stable.zip"
 else
-echo "Linux Os"
-FLUTTER_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
+  FLUTTER_DOWNLOAD_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
 fi
 PLATFORM_TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/platform-tools_r${PLATFORM_TOOLS_VERSION}-linux.zip"
 TOOLS_DOWNLOAD_LINK="https://dl.google.com/android/repository/tools_r${TOOLS_VERSION}-linux.zip"
 
+printf 'Continue ? '; read answer
 
-
-if [[ -d "$FLUTTER_VERSION_FOLDER" ]]
-then
+if [[ -d "$FLUTTER_VERSION_FOLDER" ]]; then
   echo "flutter $FLUTTER_VERSION already available .. skipping"
 else
   mkdir  -p "$FLUTTER_FOLDER" 
@@ -49,15 +48,14 @@ else
   echo "installing flutter $FLUTTER_VERSION"
   echo "printing OS $OS"
   if [ $OS = "Darwin" ];then
-  echo "unzipping the file started..."
-  echo "extracting flutter-${FLUTTER_VERSION} zip file..."
-  unzip flutter_macos_${FLUTTER_VERSION}-stable.zip
+    echo "unzipping the file started..."
+    echo "extracting flutter-${FLUTTER_VERSION} zip file..."
+    unzip flutter_macos_${FLUTTER_VERSION}-stable.zip
   else
-  echo "untaring the file started..."
-  echo "extracting flutter-${FLUTTER_VERSION} tar file..."
-  tar -xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+    echo "untaring the file started..."
+    echo "extracting flutter-${FLUTTER_VERSION} tar file..."
+    tar -xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
   fi
-  
   # TODO: is this correct?
   mv flutter $FLUTTER_VERSION_FOLDER
 fi
@@ -65,14 +63,15 @@ fi
 # Always update the current symlink
 echo "resetting current symlink"
 cd "$FLUTTER_FOLDER"
-if [[ -d "current" ]]
-then
+if [[ -d "current" ]]; then
   rm current
 fi
 ln -s $FLUTTER_VERSION_FOLDER current
 echo "flutter $FLUTTER_VERSION  .. current symlink updated"
 pwd
 cd
+
+printf 'Continue ? '; read answer
 
 # java jdk setup:
 echo "starting java jdk setup.."
@@ -82,14 +81,17 @@ cd $DEST_DIR/java
 JAVA_JDK_FOLDER="$DEST_DIR/java/jdk-$JAVA_JDK_VERSION.jdk"
 
 # download the required java jdk version 
-if [[ -a "jdk-${JAVA_JDK_TAR_VERSION}.jdk" ]]
-then
-  echo "java jdk version $JAVA_JDK_VERSION already exists..skipping"
+if [[ -d "jdk-$JAVA_JDK_TAR_VERSION" ]]; then
+  echo "java jdk version $JAVA_JDK_TAR_VERSION already exists..skipping"
 else
-echo "downloading java jdk- $JAVA_JDK_VERSION.."
-curl -LO $JAVA_JDK_URL
-echo "untaring java jdk- $JAVA_JDK_VERSION tar file..."
-tar -xf $JAVA_JDK_TAR_FILE
+  if [[ -a "$JAVA_JDK_TAR_FILE" ]]; then
+    echo "$JAVA_JDK_TAR_FILE file already exists..skipping download"
+  else
+    echo "downloading java jdk-$JAVA_JDK_VERSION .."
+    curl -LO $JAVA_JDK_URL
+  fi
+  echo "untaring java jdk- $JAVA_JDK_VERSION tar file..."
+  tar -xf $JAVA_JDK_TAR_FILE
 fi
 
 
@@ -100,13 +102,17 @@ if [[ -d "current" ]]
 then
   rm current
 fi
-ln -s jdk-${JAVA_JDK_TAR_VERSION}.jdk current
+ln -s jdk-${JAVA_JDK_TAR_VERSION} current
 echo ".. java jdk $JAVA_JDK_VERSION  .. current symlink updated"
+
+printf 'Continue ? '
+read answer
 
 # android sdk setup: 
 echo "starting android sdk setup.."
-mkdir -p $DEST_DIR/android/sdk
-cd $DEST_DIR/android/sdk
+ANDROID_SDK_HOME=$DEST_DIR/android/sdk
+mkdir -p $ANDROID_SDK_HOME
+cd $ANDROID_SDK_HOME
 pwd
 
 
@@ -115,24 +121,25 @@ pwd
 #commandlinetools download link:https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
 #cd opt/android/sdk
 echo "latest command line tools are downloading..."
-if [[ -a $CMDLINE_TOOLS_ZIP ]]
-then
+if [[ -a $CMDLINE_TOOLS_ZIP ]]; then
   echo "commandlinetools version latest version already exists..skipping"
 else
-curl -LO $CMDLINE_TOOLS_URL
-echo "unzipping the commandline tools..."
-unzip  $CMDLINE_TOOLS_ZIP 
-cd cmdline-tools/
-mkdir latest 
-mv -i * latest
+  curl -LO $CMDLINE_TOOLS_URL
+  echo "unzipping the commandline tools..."
+  unzip  $CMDLINE_TOOLS_ZIP 
+  cd $ANDROID_SDK_HOME/cmdline-tools
+  mkdir latest 
+  mv -i * latest
 fi
 echo "command line tools are ready..."
 pwd
 
+printf 'Continue ? '
+read answer
 
 
 ### required sdk tools installation command:
-cd cmdline-tools/latest/bin
+cd $ANDROID_SDK_HOME/cmdline-tools/latest/bin
 sdkmanager --list
 sdkmanager "platforms;android-$ANDROID_VERSION"
 sdkmanager "platform-tools"
--- installs/versions.sh
+++ installs/versions.sh
@@ -1,16 +1,15 @@
 # export the env variables defined here
 # run the scripts as follows:
 # (source ~/projects/github/appsmatics/linux-setup/installs/versions.sh; ~/projects/github/appsmatics/linux-setup/installs/flutter.sh)
+set -a
 
 OS=$(uname -s)
 REQUIRED_OS="linux"
 REQUIRED_JAVA_OS="linux"
-if [ $OS = "Darwin" ]; then
+if [[ $OS == "Darwin" ]]; then
   REQUIRED_OS=mac
   REQUIRED_JAVA_OS="macos"
 fi
-
-set -a
 
 DEST_DIR="/opt2"
 
@@ -20,8 +19,18 @@ FLUTTER_VERSION="3.24.3"
 ANDROID_VERSION="34"
 BUILD_TOOLS_VERSION="34.0.0"
 JAVA_JDK_DOWNLOAD_VERSION="17"
-## jdk tar file version number different 17 -> 17.0.12, 21 -> 21.0.4, 23 -> 23
+
 JAVA_JDK_TAR_VERSION="17.0.12"
+
+# https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz
+
+if [[ $REQUIRED_OS == 'mac' ]]; then
+  ## macos jdk tar file version number different 17 -> 17.0.12, 21 -> 21.0.4, 23 -> 23
+  # https://download.oracle.com/java/17/latest/jdk-17_macos-x64_bin.tar.gz
+  # https://download.oracle.com/java/17/latest/jdk-17_macos-aarch64_bin.tar.gz
+  JAVA_JDK_TAR_VERSION="17.0.12"
+
+fi
 
 CMDLINE_TOOLS_URL="https://dl.google.com/android/repository/commandlinetools-${REQUIRED_OS}-11076708_latest.zip"
 
